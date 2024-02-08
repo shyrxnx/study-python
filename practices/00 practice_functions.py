6ec922f1347e5b8508e33374be0e69ac9dd4af4d
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-08 12:59:37 +0800

    practice of using functions

---
+++ practices/00 practice_functions.py
@@ -0,0 +1,41 @@
+# spam = 0
+
+# while spam < 5:
+#     print("Hello, World!")
+#     spam += 1
+
+# number = 0
+
+# for i in range(101):
+#     number += i
+
+# print(number)
+
+
+# def div50by(divisor):
+#     try:
+#         return 50 / divisor
+#     except ZeroDivisionError:
+#         print ("Error! You tried to divide by zero.")
+
+# print(int(div50by(10)))
+# print(div50by(5))
+# print(div50by(0))
+
+
+# while True:
+#     try:
+#         numCats = (int(input("Enter the number of cats you own. Must be a positive integer.\nEnter cats owned: ")))
+#         if numCats < 0:
+#             print("Please enter a positive integer.")
+#             continue
+#         else:
+#             break
+#     except ValueError:
+#         print("Please enter a valid positive integer.")
+
+
+# if numCats >= 4:
+#     print("Holy! That's a lot of cats!")
+# else:
+#     print("Well, that's underwhelming.")
