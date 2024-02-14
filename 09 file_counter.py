44816749daf2467e119d9f17004672b2e1a1fd2d
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-14 21:42:56 +0800

    wrote a simple file and folder counter that uses recursion

--- 09 file_counter.py
+++ 09 file_counter.py
@@ -1,31 +1,30 @@
 import os
 
-# Write a program that counts the number of files in a directory (excluding directories themselves).
+# Write a program that counts the number of files and folders in a directory (excluding the directory provided itself).
 
-print(os.listdir(r"..\..\Downloads\JPCS Picture Layouts"))
-
-main_directory = r"..\..\Downloads\JPCS Picture Layouts"
-file = r"..\..\Downloads\JPCS Picture Layouts\logo.png"
-count = 0
+main_directory = r"C:\Users\Shyrine\Downloads\JPCS Picture Layouts"
 
 
-# Checks if the path given is a file or not (folder)
-def check_if_file():
-    return os.path.isfile(main_directory)
+def count_files_and_folders(directory):
+    files_count = 0
+    folders_count = 0
+
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isfile(item_path):
+            files_count += 1
+        elif os.path.isdir(item_path):
+            folders_count += 1
+
+            # Recursively count files and folders within the folder
+            files_in_folder, folder_in_folder = count_files_and_folders(item_path)
+            files_count += files_in_folder
+            folders_count += folder_in_folder
+
+    return files_count, folders_count
 
 
-if check_if_file():
-    print("The path is a file")
-else:
-    print("The path is a folder")
-
-
-# Checks if the path given is a folder or not (file)
-def check_if_folder():
-    return os.path.isdir(file)
-
-
-if check_if_folder():
-    print("The path is a folder")
-else:
-    print("The path is a file")
+# Call the function to count files and folders in the directory
+total_files, total_folders = count_files_and_folders(main_directory)
+print(f"The total files in the directory is: {total_files}")
+print(f"The total folders in the directory is: {total_folders}")
