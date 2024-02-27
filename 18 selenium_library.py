fd210db1794f866eeddbc84275c28a623704b452
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-27 10:59:51 +0800

    feat: Made a python script that uses Selenium Webdriver to search in YouTube

--- 18 selenium_library.py
+++ 18 selenium_library.py
@@ -3,11 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
-driver.get("https://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
+driver.get("https://www.youtube.com/")
+search = driver.find_element(By.NAME, "search_query")
+search.click()
+search.clear()
+search.send_keys("Pantropiko by BINI")
+search.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-driver.close()
