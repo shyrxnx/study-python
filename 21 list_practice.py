846ecb3ff0655219769f8fc24f0d87162ac78a87
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-03-03 22:02:03 +0800

    feat: Implemented sorting in the array

---
+++ 21 list_practice.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "orange", "watermelon", "grape", "cherries", "mango"]
+
+# Iterates over the array and prints each fruit in every iteration
+for fruit in fruits:
+    print(fruit)
+
+# Sorts the array in alphabetical order
+fruits.sort()
+print(fruits)
+
+# Sorts the array in reverse alphabetical order
+fruits.sort(reverse=True)
+print(fruits)
+
