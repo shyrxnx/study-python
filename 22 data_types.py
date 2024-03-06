4c307cf159efeafa60689537309a014a3b90ca66
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-03-06 19:41:05 +0800

    feat: Demonstrate implicit casting in input() function usage

---
+++ 22 data_types.py
@@ -0,0 +1,11 @@
+# Whenever you store a data on a variable with no explicit casting, the data type of that is string
+
+name = input("Enter your name: ")
+print(f"Hello {name}")
+print(type(name)) # Checks the data type
+
+# Example No. 2
+num1 = input("Enter number 1: ")
+print(type(num1))
+# This will output a string
+
